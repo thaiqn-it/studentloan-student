9973d8d114938bdebde74963b887494280605ed6
dpcuong2 <dinhphucuong2@gmail.com>
2021-12-25 18:20:41 +0700

    change landing page layout

--- src/pages/LandingPage/index.js
+++ src/pages/LandingPage/index.js
@@ -6,6 +6,8 @@ import {
   Typography,
   Slider,
   Box,
+  Icon,
+  Checkbox,
 } from "@mui/material";
 import { ReactComponent as ReactLogo } from "../../images/LandingPageIllustration.svg";
 import Footer from "../../components/Footer";
@@ -39,104 +41,179 @@ const LandingPage = (props) => {
 
   return (
     <>
-      <div className={classes.container}>
+      <Box className={classes.container}>
         <NavBar
           display={true}
           isLoggedIn={isLoggedIn}
           changeIsLogged={(isLog) => setIsLoggedIn(isLog)}
         />
-        <Box>
-          <Container>
-            <Grid container spacing={2}>
-              <Grid item xs={4}>
-                <div className={classes.landingContent}>
-                  <Typography variant="h2">Landing Page Sologan</Typography>
-                  <Typography variant="body1" className={classes.text}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Vestibulum libero neque, mollis a risus et, pharetra
-                    hendrerit velit. Aliquam vitae pretium mauris. Praesent
-                    fermentum tortor nec molestie posuere. Sed at pretium arcu.
-                    Sed id tempus velit, sit amet sollicitudin lectus. Phasellus
-                    vel nisl pharetra, scelerisque sapien eget, aliquam libero.
-                    Mauris accumsan felis vel metus luctus finibus. Fusce
-                    ullamcorper mi in quam fermentum molestie nec a tortor.
-                    Vestibulum commodo leo est, quis volutpat nibh imperdiet
-                    quis.
-                  </Typography>
-                  <div>
-                    <Button
-                      variant="contained"
-                      sx={{
-                        backgroundColor: "#338a3e",
-                      }}
-                      size="large"
-                    >
-                      <Link className={classes.linkButton}>Get Started</Link>
-                    </Button>
-                  </div>
-                </div>
-              </Grid>
-              <Grid
-                item
-                xs={8}
-                style={{ display: "flex", justifyContent: "center" }}
-              >
-                <ReactLogo width="500" />
-              </Grid>
-            </Grid>
-          </Container>
 
-          <Box className={classes.moneyBox}>
-            <Box className={classes.moneyWrapper}>
-              <Box
-                style={{
-                  display: "flex",
-                  flexDirection: "column",
-                  padding: "15px",
+        <Container>
+          <Grid container spacing={2}>
+            <Grid item xs={4}>
+              <Box className={classes.landingContent}>
+                <Typography variant="h2">Landing Page Sologan</Typography>
+                <Typography variant="body1" className={classes.text}>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Vestibulum libero neque, mollis a risus et, pharetra hendrerit
+                  velit. Aliquam vitae pretium mauris. Praesent fermentum tortor
+                  nec molestie posuere. Sed at pretium arcu. Sed id tempus
+                  velit, sit amet sollicitudin lectus. Phasellus vel nisl
+                  pharetra, scelerisque sapien eget, aliquam libero. Mauris
+                  accumsan felis vel metus luctus finibus. Fusce ullamcorper mi
+                  in quam fermentum molestie nec a tortor. Vestibulum commodo
+                  leo est, quis volutpat nibh imperdiet quis.
+                </Typography>
+                <Box>
+                  <Button
+                    variant="contained"
+                    sx={{
+                      backgroundColor: "#338a3e",
+                    }}
+                    size="large"
+                  >
+                    <Link className={classes.linkButton} color="inherit">
+                      Get Started
+                    </Link>
+                  </Button>
+                </Box>
+              </Box>
+            </Grid>
+            <Grid
+              item
+              xs={8}
+              style={{ display: "flex", justifyContent: "center" }}
+            >
+              <ReactLogo width="500" />
+            </Grid>
+          </Grid>
+        </Container>
+
+        <Box className={classes.moneyBox}>
+          <Box className={classes.moneyWrapper}>
+            <Box
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                padding: "15px",
+              }}
+            >
+              <Typography variant="h3">How much do you need</Typography>
+              <Typography
+                variant="h4"
+                sx={{ alignSelf: "center" }}
+              >{`${sliderValue}`}</Typography>
+              <Slider
+                aria-label="c"
+                value={typeof sliderValue === "number" ? sliderValue : 0}
+                defaultValue={2}
+                step={0.5}
+                min={2}
+                max={50}
+                marks={marks}
+                onChange={sliderValueChangeHandler}
+              />
+              <Button
+                variant="contained"
+                sx={{
+                  backgroundColor: "#338a3e",
+                  width: "40%",
+                  alignSelf: "center",
                 }}
+                size="large"
               >
-                <Typography variant="h3">How much do you need</Typography>
-                <Typography
-                  variant="h4"
-                  sx={{ alignSelf: "center" }}
-                >{`${sliderValue}`}</Typography>
-                <Slider
-                  aria-label="c"
-                  value={typeof sliderValue === "number" ? sliderValue : 0}
-                  defaultValue={2}
-                  step={0.5}
-                  min={2}
-                  max={50}
-                  marks={marks}
-                  onChange={sliderValueChangeHandler}
-                />
-                <Button
-                  variant="contained"
-                  sx={{
-                    backgroundColor: "#338a3e",
-                    width: "40%",
-                    alignSelf: "center",
-                  }}
-                  size="large"
-                >
-                  <Link className={classes.linkButton}>Apply</Link>
-                </Button>
-              </Box>
-              <Box className={classes.paymentInfo}>
-                <Typography variant="h4">Example Payment</Typography>
-                <Typography variant="h3">200k</Typography>
-                <Typography variant="h6">
-                  Repayment a month whilst studying
-                </Typography>
-                <Typography variant="h3">{`${monthlyPayment}k`}</Typography>
-                <Typography variant="h6">
-                  Repayment a month once graduated
-                </Typography>
-              </Box>
+                <Link className={classes.linkButton}>Apply</Link>
+              </Button>
+            </Box>
+            <Box className={classes.paymentInfo}>
+              <Typography variant="h4">Example Payment</Typography>
+              <Typography variant="h3">200k</Typography>
+              <Typography variant="h6">
+                Repayment a month whilst studying
+              </Typography>
+              <Typography variant="h3">{`${monthlyPayment}k`}</Typography>
+              <Typography variant="h6">
+                Repayment a month once graduated
+              </Typography>
             </Box>
           </Box>
         </Box>
-      </div>
+
+        <Box className={classes.infoSection}>
+          <Grid container>
+            <Grid item xs={3}></Grid>
+          </Grid>
+
+          <Box className={classes.infoBox}>
+            <Typography variant="h6"></Typography>
+
+            <Typography variant="h6">
+              Create pages with ease with a help of the best page builder
+              plugin.
+            </Typography>
+
+            <Button variant="outlined">Read more</Button>
+            <Typography variant="h6">Corporate Loan</Typography>
+          </Box>
+        </Box>
+
+        <Box className={classes.founder}>
+          <img src="https://i.pinimg.com/originals/57/3a/8e/573a8eb073f0a8f98e85559c1dc5214d.jpg" />
+          <Typography variant="h4">Some thing about speed</Typography>
+          <Typography variant="body1">
+            Our menu is a nod tostreet food vendorswho help their customers stay
+            on-the-go by delivering quick tasty bites. We incorporated flavors
+            from around the world to offer a unique menu featuring items like
+            Banh Mi, a flavorful sandwich staple from Vietnam, and the classic
+            American Burger.
+          </Typography>
+
+          <Checkbox defaultChecked />
+          <Typography>Fast aaa</Typography>
+        </Box>
+        <Box className={classes.service}>
+          <Typography>Our Sevice</Typography>
+          <Box className={classes.serviceBox}>
+            <Icon />
+            <Typography>Home Loan</Typography>
+            <Typography>
+              Lorem ipsum dolor sit amet consecte adipiscing elit sed do
+              eiusincidunt.
+            </Typography>
+          </Box>
+        </Box>
+        <Box className={classes.practice}>
+          <Typography>Practice</Typography>
+        </Box>
+        <Box className={classes.process}>
+          <img src="https://metropolitanhost.com/themes/themeforest/react/loanly/assets/images/whyus1.jpg" />
+          <Typography>What does Student Loan Do</Typography>
+
+          <Typography>Giấy tiwf rõ ràng</Typography>
+          <Icon />
+          <Typography>Lorm Ípun</Typography>
+        </Box>
+        <Box className={classes.team}>
+          <Typography>Our Team </Typography>
+          <img />
+          <Typography> name </Typography>
+          <Typography>Job</Typography>
+        </Box>
+
+        <Box className={classes.client}>
+          <Typography>Đối Tác</Typography>
+          <Typography>Đối Tác</Typography>
+          <Grid container>
+            <Grid item> Icon</Grid>
+          </Grid>
+        </Box>
+        <Box className={classes.faq}>
+          <Box className={classes.question}>
+            <Typography>Question</Typography>
+            <Typography>Answer</Typography>
+          </Box>
+        </Box>
+      </Box>
 
       <Footer />
     </>
