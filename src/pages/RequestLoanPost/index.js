a7c5b8f47d730b3ce656aa549a8f8b8015650db4
dpcuong2 <dinhphucuong2@gmail.com>
2022-01-07 19:41:31 +0700

    remove dashBoardLayouComponent

--- src/pages/RequestLoanPost/index.js
+++ src/pages/RequestLoanPost/index.js
@@ -53,89 +53,86 @@ export default function RequestLoanPost() {
   };
 
   return (
-    <DashboardLayout>
-      <DashboardNavbar />
-      <SuiBox py={3}>
-        <SuiBox mb={3}>
-          <Paper
-            sx={{
-              padding: "10px 0px",
-              margin: "1rem",
-              marginRight: "10px",
-              borderRadius: 5,
-            }}
-            elevation={6}
-          >
-            <Box sx={{ width: "100%" }}>
-              <Stepper nonLinear activeStep={activeStep} alternativeLabel>
-                {steps.map((label, index) => (
-                  <Step key={label} completed={completed[index]}>
-                    <StepButton onClick={handleStep(index)}>{label}</StepButton>
-                  </Step>
-                ))}
-              </Stepper>
+    <SuiBox py={3}>
+      <SuiBox mb={3}>
+        <Paper
+          sx={{
+            padding: "10px 0px",
+            margin: "1rem",
+            marginRight: "10px",
+            borderRadius: 5,
+          }}
+          elevation={6}
+        >
+          <Box sx={{ width: "100%" }}>
+            <Stepper nonLinear activeStep={activeStep} alternativeLabel>
+              {steps.map((label, index) => (
+                <Step key={label} completed={completed[index]}>
+                  <StepButton onClick={handleStep(index)}>{label}</StepButton>
+                </Step>
+              ))}
+            </Stepper>
 
-              {activeStep === 0 ? (
-                <PostInfoPage handleStep={(index) => handleStep(index)} />
-              ) : null}
-              {activeStep === 1 ? (
-                <ArchievementPage handleStep={(index) => handleStep(index)} />
-              ) : null}
-              {activeStep === 2 ? <ConfirmPage /> : null}
-              {activeStep === 3 ? <ThankyouPage /> : null}
+            {activeStep === 0 ? (
+              <PostInfoPage handleStep={(index) => handleStep(index)} />
+            ) : null}
+            {activeStep === 1 ? (
+              <ArchievementPage handleStep={(index) => handleStep(index)} />
+            ) : null}
+            {activeStep === 2 ? <ConfirmPage /> : null}
+            {activeStep === 3 ? <ThankyouPage /> : null}
 
-              <Container sx={{ padding: "3rem 3rem", display: "flex" }}>
-                {activeStep === 2 ? (
-                  <>
-                    <Button
-                      variant="contained"
-                      color="secondary"
-                      sx={{
-                        margin: "0 auto",
-                        display: "block",
-                        textTransform: "none",
-                        marginRight: "0",
-                        backgroundColor: "#f44336",
-                      }}
-                      size="large"
-                    >
-                      Send form
-                    </Button>
-                  </>
-                ) : (
-                  // <Button
-                  //   variant="contained"
-                  //   color="secondary"
-                  //   sx={{
-                  //     margin: "0 auto",
-                  //     display: "block",
-                  //     textTransform: "none",
-                  //     marginRight: "0",
-                  //     backgroundColor: "#335188",
-                  //   }}
-                  //   onClick={handleStep(activeStep + 1)}
-                  //   size="large"
-                  // >
-                  //   Next
-                  // </Button>
-                  <SuiButton
-                    color="dark"
-                    size="large"
+            <Container sx={{ padding: "3rem 3rem", display: "flex" }}>
+              {activeStep === 2 ? (
+                <>
+                  <Button
+                    variant="contained"
+                    color="secondary"
                     sx={{
                       margin: "0 auto",
                       display: "block",
                       textTransform: "none",
                       marginRight: "0",
+                      backgroundColor: "#f44336",
                     }}
+                    size="large"
                   >
-                    Next
-                  </SuiButton>
-                )}
-              </Container>
-            </Box>
-          </Paper>
-        </SuiBox>
+                    Send form
+                  </Button>
+                </>
+              ) : (
+                // <Button
+                //   variant="contained"
+                //   color="secondary"
+                //   sx={{
+                //     margin: "0 auto",
+                //     display: "block",
+                //     textTransform: "none",
+                //     marginRight: "0",
+                //     backgroundColor: "#335188",
+                //   }}
+                //   onClick={handleStep(activeStep + 1)}
+                //   size="large"
+                // >
+                //   Next
+                // </Button>
+                <SuiButton
+                  color="dark"
+                  size="large"
+                  sx={{
+                    margin: "0 auto",
+                    display: "block",
+                    textTransform: "none",
+                    marginRight: "0",
+                  }}
+                >
+                  Next
+                </SuiButton>
+              )}
+            </Container>
+          </Box>
+        </Paper>
       </SuiBox>
-    </DashboardLayout>
+    </SuiBox>
   );
 }
