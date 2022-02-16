cc605e03b3412b01db11eee1c2d5ef0bdf68da4a
dpcuong2 <dinhphucuong2@gmail.com>
2022-02-16 11:47:15 +0700

    localize and implement api to login page

--- src/layouts/authentication/sign-in/index.js
+++ src/layouts/authentication/sign-in/index.js
@@ -13,88 +13,159 @@ Coded by www.creative-tim.com
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 */
 
-import { useState } from "react";
+import { useState } from 'react'
 
 // react-router-dom components
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom'
 
 // @mui material components
-import Switch from "@mui/material/Switch";
+import Switch from '@mui/material/Switch'
 
 // Soft UI Dashboard React components
-import SuiBox from "components/SuiBox";
-import SuiTypography from "components/SuiTypography";
-import SuiInput from "components/SuiInput";
-import SuiButton from "components/SuiButton";
+import SuiBox from 'components/SuiBox'
+import SuiTypography from 'components/SuiTypography'
+import SuiInput from 'components/SuiInput'
+import SuiButton from 'components/SuiButton'
 
 // Authentication layout components
-import CoverLayout from "layouts/authentication/components/CoverLayout";
+import CoverLayout from 'layouts/authentication/components/CoverLayout'
 
 // Images
-import curved9 from "assets/images/curved-images/curved-6.jpg";
+import curved9 from 'assets/images/curved-images/curved-6.jpg'
+import { loginUser } from 'context/userAction'
+import { useHistory } from 'react-router-dom'
 
 function SignIn() {
-  const [rememberMe, setRememberMe] = useState(true);
+    const [rememberMe, setRememberMe] = useState(true)
 
-  const handleSetRememberMe = () => setRememberMe(!rememberMe);
+    const handleSetRememberMe = () => setRememberMe(!rememberMe)
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [error, setError] = useState(false)
+    const errorMessage = 'Tài Khoản Đăng Nhập Hoặc Mật Khẩu Bị Sai'
+    const history = useHistory()
 
-  return (
-    <CoverLayout
-      title="Welcome back"
-      description="Enter your email and password to sign in"
-      image={curved9}
-    >
-      <SuiBox component="form" role="form">
-        <SuiBox mb={2}>
-          <SuiBox mb={1} ml={0.5}>
-            <SuiTypography component="label" variant="caption" fontWeight="bold">
-              Email
-            </SuiTypography>
-          </SuiBox>
-          <SuiInput type="email" placeholder="Email" />
-        </SuiBox>
-        <SuiBox mb={2}>
-          <SuiBox mb={1} ml={0.5}>
-            <SuiTypography component="label" variant="caption" fontWeight="bold">
-              Password
-            </SuiTypography>
-          </SuiBox>
-          <SuiInput type="password" placeholder="Password" />
-        </SuiBox>
-        <SuiBox display="flex" alignItems="center">
-          <Switch checked={rememberMe} onChange={handleSetRememberMe} />
-          <SuiTypography
-            variant="button"
-            fontWeight="regular"
-            onClick={handleSetRememberMe}
-            sx={{ cursor: "pointer", userSelect: "none" }}
-          >
-            &nbsp;&nbsp;Remember me
-          </SuiTypography>
-        </SuiBox>
-        <SuiBox mt={4} mb={1}>
-          <SuiButton variant="gradient" color="info" fullWidth>
-            sign in
-          </SuiButton>
-        </SuiBox>
-        <SuiBox mt={3} textAlign="center">
-          <SuiTypography variant="button" color="text" fontWeight="regular">
-            Don&apos;t have an account?{" "}
-            <SuiTypography
-              component={Link}
-              to="/authentication/sign-up"
-              variant="button"
-              color="info"
-              fontWeight="medium"
-              textGradient
-            >
-              Sign up
-            </SuiTypography>
-          </SuiTypography>
-        </SuiBox>
-      </SuiBox>
-    </CoverLayout>
-  );
+    const signIn = async (e) => {
+        try {
+            console.log('login')
+            e.preventDefault()
+            setError(false)
+            const response = await loginUser(dispatch, email, password)
+            if (!response.data.id) return setError(errorMessage)
+            history.push('/dashboard')
+        } catch (err) {
+            //handle Login
+            setError(true)
+        }
+    }
+
+    const handleInputEmail = (e) => {
+        setEmail(e.target.value)
+    }
+
+    const handleInputPassword = (e) => {
+        setPassword(e.target.value)
+    }
+
+    return (
+        <CoverLayout
+            title="Đăng Nhập"
+            description="Xin hãy nhập email và mật khẩu để đăng nhập"
+            image={curved9}
+        >
+            <SuiBox component="form" role="form">
+                <SuiBox mb={2}>
+                    <SuiBox mb={1} ml={0.5}>
+                        <SuiTypography
+                            component="label"
+                            variant="caption"
+                            fontWeight="bold"
+                        >
+                            Email
+                        </SuiTypography>
+                    </SuiBox>
+                    <SuiInput
+                        type="email"
+                        placeholder="Email"
+                        error={error}
+                        value={email}
+                        onChange={handleInputEmail}
+                    />
+                </SuiBox>
+                <SuiBox mb={2}>
+                    <SuiBox mb={1} ml={0.5}>
+                        <SuiTypography
+                            component="label"
+                            variant="caption"
+                            fontWeight="bold"
+                        >
+                            Mật Khẩu
+                        </SuiTypography>
+                    </SuiBox>
+                    <SuiInput
+                        type="password"
+                        placeholder="Mật Khẩu"
+                        error={error}
+                        value={password}
+                        onChange={handleInputPassword}
+                    />
+                    {error && (
+                        <SuiTypography
+                            component="label"
+                            variant="caption"
+                            fontWeight="bold"
+                            color="error"
+                        >
+                            {errorMessage}
+                        </SuiTypography>
+                    )}
+                </SuiBox>
+                <SuiBox display="flex" alignItems="center">
+                    <Switch
+                        checked={rememberMe}
+                        onChange={handleSetRememberMe}
+                    />
+                    <SuiTypography
+                        variant="button"
+                        fontWeight="regular"
+                        onClick={handleSetRememberMe}
+                        sx={{ cursor: 'pointer', userSelect: 'none' }}
+                    >
+                        &nbsp;&nbsp;Ghi nhớ đăng nhập
+                    </SuiTypography>
+                </SuiBox>
+                <SuiBox mt={4} mb={1}>
+                    <SuiButton
+                        variant="gradient"
+                        color="info"
+                        fullWidth
+                        onClick={signIn}
+                    >
+                        Đăng Nhập
+                    </SuiButton>
+                </SuiBox>
+                <SuiBox mt={3} textAlign="center">
+                    <SuiTypography
+                        variant="button"
+                        color="text"
+                        fontWeight="regular"
+                    >
+                        Chưa có tài khoảng ?
+                        <SuiTypography
+                            component={Link}
+                            to="/authentication/sign-up"
+                            variant="button"
+                            color="info"
+                            fontWeight="medium"
+                            textGradient
+                        >
+                            Đăng kí
+                        </SuiTypography>
+                    </SuiTypography>
+                </SuiBox>
+            </SuiBox>
+        </CoverLayout>
+    )
 }
 
-export default SignIn;
+export default SignIn
